bd16732484b03d04433a17d0b9b2ead1be6a9e86
Jiii-Eun <mh030231@gmail.com>
2025-08-29 04:14:41 +0900

    tailwind 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <div>
       <h1 className="">Tailwind 테스트</h1>
-      <button className="mt-4 rounded bg-violet-500 px-4 py-2 text-white hover:bg-violet-600">
+      <button className="mt-4 rounded-sm bg-violet-500 px-4 py-2 text-white hover:bg-violet-600">
         버튼 테스트
       </button>
-      <div className="text-3xl-bold bg-brandGray-100">Tailwind 커스텀 확인</div>
+      <div className="text-3xl-bold bg-brand-gray-100">Tailwind 커스텀 확인</div>
     </div>
   );
 }
