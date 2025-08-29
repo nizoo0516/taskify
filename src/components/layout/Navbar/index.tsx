904a465ccace0f4ddd19941bed8b0f52a45421ef
Jiii-Eun <mh030231@gmail.com>
2025-08-29 16:56:47 +0900

    임시저장

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -1,7 +1,19 @@
-export default function Navbar() {
+export default function Navbar({ title = "내 대시보드" }) {
   return (
     <>
-      <div className=""></div>
+      <div className="fixed top-0 right-0 bg-white">
+        <div className="flex justify-between">
+          <h2 className="text-xl-bold">{title}</h2>
+          <div>
+            <button>관리</button>
+            <button>초대하기</button>
+            <ol>
+              {members.length > 0 &&
+                memvers.map((member) => <li key={member.id}>{member.profileImageUrl}</li>)}
+            </ol>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
