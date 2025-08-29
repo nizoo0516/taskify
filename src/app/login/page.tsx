a7622db3de085144d8d2ef2272b81a57cbd57170
Jiii-Eun <mh030231@gmail.com>
2025-08-29 18:33:22 +0900

    Style: 테스트 코드 삭제

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,29 +1,7 @@
-"use client";
-
-import { useState } from "react";
-
-import { login } from "@/features/auth/api";
-
-export default function LoginPage() {
-  const [email, setEmail] = useState("example@email.com");
-  const [password, setPassword] = useState("12345678");
-
-  const handleLogin = async () => {
-    try {
-      const res = await login({ email, password });
-      console.log("로그인 성공:", res.accessToken);
-      alert("로그인 성공!");
-    } catch (err) {
-      console.error("로그인 실패:", err);
-    }
-  };
-
+export default function Page() {
   return (
-    <div>
-      <h1>로그인 페이지</h1>
-      <input value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={handleLogin}>로그인</button>
-    </div>
+    <>
+      <div className=""></div>
+    </>
   );
 }
