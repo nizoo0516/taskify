16e54e37c39123ed3750e5edb2bf90482e5909b0
Jiii-Eun <mh030231@gmail.com>
2025-08-29 17:46:06 +0900

    Feat: 디바이스 훅추가 및 api훅 수정

--- src/lib/useApiHandler.ts
+++ src/lib/useApiHandler.ts
@@ -1,12 +1,13 @@
-import { useEffect, useState } from "react";
+import { DependencyList, useEffect, useState } from "react";
 
-export function useApiHandler(apiFn, deps = []) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+export function useApiHandler<T>(apiFn: () => Promise<T>, deps: DependencyList = []) {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
     let isMounted = true;
+
     async function fetchData() {
       setLoading(true);
       setError(null);
@@ -14,16 +15,18 @@ export function useApiHandler(apiFn, deps = []) {
         const res = await apiFn();
         if (isMounted) setData(res);
       } catch (err) {
-        if (isMounted) setError(err);
+        if (isMounted) setError(err as Error);
       } finally {
         if (isMounted) setLoading(false);
       }
     }
+
     fetchData();
+
     return () => {
       isMounted = false;
     };
-  }, deps);
+  }, [apiFn, ...deps]);
 
   return { data, loading, error };
 }
