273ddb2407f6f59c6df6efba2b907d0bc64920e1
apple <apple@apples-MacBook-Air.local>
2025-08-30 14:45:00 +0900

    250830 button component add

--- src/app/mydashboard/page.tsx
+++ src/app/mydashboard/page.tsx
@@ -1,7 +1,13 @@
+"use client"; 
+
+import MyButton from "@/components/layout/Button";
+
 export default function MyDashboard() {
   return (
-    <>
-      <div className=""></div>
-    </>
+    <div>
+      <MyButton text="새로운 대시보드" className="py-2.5 px-5 me-2 mb-2 text-[#787486] w-[160px] h-[60px]" onClick={() => alert("대시보드 생성!")}/>
+      <MyButton text="로그인" className="py-2.5 px-5 me-2 mb-2 text-[#FFF]" color="buttonBlue" onClick={() => alert("대시보드 생성!")}/>
+      <MyButton text="로그인" className="py-2.5 px-5 me-2 mb-2 text-[#FFF]" color="buttonGrey" onClick={() => alert("대시보드 생성!")}/>
+    </div>
   );
 }
