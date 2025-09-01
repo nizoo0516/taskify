b36f1ba94fdba167d45edb7f8f02a1f3e3004132
Jiii-Eun <mh030231@gmail.com>
2025-09-01 15:48:50 +0900

    style: 폰트 폴더위치 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,19 +4,19 @@ import "@/styles/globals.css";
 
 const pretendard = localFont({
   src: [
-    { path: "../assets/fonts/Pretendard-Regular.woff2", weight: "400" },
-    { path: "../assets/fonts/Pretendard-Medium.woff2", weight: "500" },
-    { path: "../assets/fonts/Pretendard-SemiBold.woff2", weight: "600" },
-    { path: "../assets/fonts/Pretendard-Bold.woff2", weight: "700" },
-    { path: "../assets/fonts/Pretendard-Black.woff2", weight: "900" },
+    { path: "/fonts/Pretendard-Regular.woff2", weight: "400" },
+    { path: "/fonts/Pretendard-Medium.woff2", weight: "500" },
+    { path: "/fonts/Pretendard-SemiBold.woff2", weight: "600" },
+    { path: "/fonts/Pretendard-Bold.woff2", weight: "700" },
+    { path: "/fonts/Pretendard-Black.woff2", weight: "900" },
   ],
   variable: "--font-sans",
 });
 
 const montserrat = localFont({
   src: [
-    { path: "../assets/fonts/Montserrat-SemiBold.ttf", weight: "600" },
-    { path: "../assets/fonts/Montserrat-Bold.ttf", weight: "700" },
+    { path: "/fonts/Montserrat-SemiBold.ttf", weight: "600" },
+    { path: "/fonts/Montserrat-Bold.ttf", weight: "700" },
   ],
   variable: "--font-eng",
 });
