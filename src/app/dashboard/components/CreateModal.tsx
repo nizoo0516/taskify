d259d0a8f90baac648bd42e5004da7180a5403c4
Jiii-Eun <mh030231@gmail.com>
2025-09-03 17:12:16 +0900

    Fix: 파일 주석처리

--- src/app/dashboard/components/CreateModal.tsx
+++ src/app/dashboard/components/CreateModal.tsx
@@ -1,71 +1,71 @@
-"use client";
+// "use client";
 
-import Chip from "@/components/chip/Chip";
-import Field from "@/components/form/Field";
-// import ImgUpload from "@/components/form/ImgUpload";
-import Input from "@/components/form/Input";
-import Select from "@/components/form/Select";
-import Textarea from "@/components/form/Textarea";
-import Button from "@/components/layout/Button";
-import { Modal, ModalHeader, ModalContext, ModalFooter } from "@/components/Modal";
+// import Chip from "@/components/chip/Chip";
+// import Field from "@/components/form/Field";
+// // import ImgUpload from "@/components/form/ImgUpload";
+// import Input from "@/components/form/Input";
+// import Select from "@/components/form/Select";
+// import Textarea from "@/components/form/Textarea";
+// import Button from "@/components/layout/Button";
+// import { Modal, ModalHeader, ModalContext, ModalFooter } from "@/components/Modal";
 
-// import { DatePicker } from "../../../../components/ui/dataPicker";
+// // import { DatePicker } from "../../../../components/ui/dataPicker";
 
-type ModalType = {
-  isOpen: boolean;
-  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
-};
+// type ModalType = {
+//   isOpen: boolean;
+//   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+// };
 
-const managerOpt = [
-  { value: "1", label: "배유철", chip: <Chip variant="color" label="B" /> },
-  { value: "2", label: "배동석" },
-];
+// const managerOpt = [
+//   { value: "1", label: "배유철", chip: <Chip variant="color" label="B" /> },
+//   { value: "2", label: "배동석" },
+// ];
 
-export default function CreateModal({ isOpen, setIsOpen }: ModalType) {
-  return (
-    <div>
-      {isOpen && (
-        <Modal open={isOpen} isOpenModal={setIsOpen} size="lg">
-          <ModalHeader title="할 일 생성" />
-          <ModalContext className="flex flex-col gap-7">
-            <Field id="manager" label="담당자">
-              <Select options={managerOpt} placeholder="선택하기" />
-            </Field>
-            <Field id="title" label="제목">
-              <Input />
-            </Field>
-            <Field id="description" label="설명">
-              <Textarea className="resize-none" />
-            </Field>
-            {/* <Field id="endDate" label="마감일">
-              <DatePicker />
-            </Field> */}
-            <Field id="tag" label="태그">
-              <Input />
-            </Field>
-            {/* <Field id="image" label="이미지">
-              <ImgUpload />
-            </Field> */}
-          </ModalContext>
-          <ModalFooter>
-            <Button
-              className="h-[54px] w-64"
-              onClick={() => {
-                setIsOpen(false);
-              }}
-            >
-              취소
-            </Button>
-            <Button
-              className="text-brand-gray-100 h-[54px] w-64"
-              onClick={() => {}}
-              color="buttonBlue"
-            >
-              생성
-            </Button>
-          </ModalFooter>
-        </Modal>
-      )}
-    </div>
-  );
-}
+// export default function CreateModal({ isOpen, setIsOpen }: ModalType) {
+//   return (
+//     <div>
+//       {isOpen && (
+//         <Modal open={isOpen} isOpenModal={setIsOpen} size="lg">
+//           <ModalHeader title="할 일 생성" />
+//           <ModalContext className="flex flex-col gap-7">
+//             <Field id="manager" label="담당자">
+//               <Select options={managerOpt} placeholder="선택하기" />
+//             </Field>
+//             <Field id="title" label="제목">
+//               <Input />
+//             </Field>
+//             <Field id="description" label="설명">
+//               <Textarea className="resize-none" />
+//             </Field>
+//             {/* <Field id="endDate" label="마감일">
+//               <DatePicker />
+//             </Field> */}
+//             <Field id="tag" label="태그">
+//               <Input />
+//             </Field>
+//             {/* <Field id="image" label="이미지">
+//               <ImgUpload />
+//             </Field> */}
+//           </ModalContext>
+//           <ModalFooter>
+//             <Button
+//               className="h-[54px] w-64"
+//               onClick={() => {
+//                 setIsOpen(false);
+//               }}
+//             >
+//               취소
+//             </Button>
+//             <Button
+//               className="text-brand-gray-100 h-[54px] w-64"
+//               onClick={() => {}}
+//               color="buttonBlue"
+//             >
+//               생성
+//             </Button>
+//           </ModalFooter>
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
