ad2a297c713a3a979ab3d34faefd02b660999c45
Jiii-Eun <mh030231@gmail.com>
2025-09-04 21:15:03 +0900

    Design: 메인 디자인 완성

--- src/components/home/ExtraFeatures.tsx
+++ src/components/home/ExtraFeatures.tsx
@@ -1,7 +1,44 @@
 export default function ExtraFeatures() {
+  const extraList = [
+    {
+      id: "extra1",
+      src: "/images/img-main-extra1.png",
+      title: "대시보드 설정",
+      desc: "대시보드 사진과 이름을 변경할 수 있어요.",
+    },
+    {
+      id: "extra2",
+      src: "/images/img-main-extra2.png",
+      title: "초대",
+      desc: "새로운 팀원을 초대할 수 있어요.",
+    },
+    {
+      id: "extra3",
+      src: "/images/img-main-extra3.png",
+      title: "구성원",
+      desc: "구성원을 초대하고 내보낼 수 있어요.",
+    },
+  ];
+
   return (
-    <>
-      <div className=""></div>
-    </>
+    <div>
+      <div className="tablet:mb-9 mb-10 text-center text-[22px] font-bold">
+        생산성을 높이는 다양한 설정 ⚡
+      </div>
+
+      <div className="pc:flex-row pc:gap-8 tablet:gap-12 flex flex-col items-center gap-10">
+        {extraList.map((item) => (
+          <div key={item.id} className="tablet:max-w-[378px] max-w-[343px]">
+            <div className="bg-brand-gray-600 tablet:h-[260px] tablet:px-[39px] flex h-[236px] items-center justify-center rounded-t-lg px-[42px]">
+              <img src={item.src} alt="페이지 기능 이미지" />
+            </div>
+            <div className="tablet:py-8 bg-brand-gray-800 rounded-b-lg px-8 py-7">
+              <div className="text-2lg mb-4 font-bold">{item.title}</div>
+              <div className="text-lg font-medium">{item.desc}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+
+import LoginButton from "./LoginButton";
+
 export default function Hero() {
   return (
     <>
-      <div className=""></div>
+      <div>
+        <div className="tablet:h-80 pc:h-[422px] tablet:mt-20 tablet:mb-12 relative mt-11 mb-7 h-40 w-full">
+          <Image src={"/images/img-main.svg"} alt="메인 이미지" fill />
+        </div>
+        <div className="tablet:flex-row tablet:gap-7 tablet:mb-28 mb-24 flex flex-col items-center justify-center gap-1.5">
+          <div className="pc:text-7xl tablet:text-[56px] text-[40px] leading-none font-bold">
+            새로운 일정관리
+          </div>
+          <div className="font-eng text-brand-blue-500 tablet:text-7xl pc:text-[90px] text-[42px] leading-none font-bold">
+            Taskify
+          </div>
+        </div>
+        <LoginButton />
+      </div>
     </>
   );
 }
