686639c93417e41883deec751802f6c96df5e3f8
Jiii-Eun <mh030231@gmail.com>
2025-09-04 21:15:03 +0900

    Design: 메인 디자인 완성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
+import ExtraFeatures from "@/components/home/ExtraFeatures";
+import Features from "@/components/home/Features";
+import Hero from "@/components/home/Hero";
 import Footer from "@/components/layout/home/Footer";
 import Header from "@/components/layout/home/Header";
-import TestLogin from "@/components/layout/home/TestLogin";
+// import TestLogin from "@/components/layout/home/TestLogin";
 
-export default function LoginButton() {
+export default function Home() {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-black text-white">
+    <div className="flex min-h-screen w-full flex-col justify-center bg-black text-white">
       <Header />
-      <main className="flex-1">
-        <TestLogin />
+      <main className="flex flex-1 justify-center">
+        {/* <TestLogin /> */}
+        <div className="pc:w-[1200px] tablet:w-[664px] w-[343px]">
+          <Hero />
+          <Features />
+          <ExtraFeatures />
+        </div>
       </main>
-      <div>
-        <Footer />
-      </div>
+      <Footer />
     </div>
   );
 }
--- src/components/home/Features.tsx
+++ src/components/home/Features.tsx
@@ -1,7 +1,58 @@
+import { cn } from "@/lib/utils/cn";
+
 export default function Features() {
+  const pointList = [
+    {
+      point: "Point 1",
+      desc: (
+        <>
+          일의 <span className="text-brand-gray-300">우선순위</span>를
+          <br />
+          관리하세요
+        </>
+      ),
+      src: "/images/img-main-point1.png",
+      cardStyle: cn("pc:flex-row"),
+      imgStyle: cn("tablet:pl-32 pl-12 pc:pl-0"),
+    },
+    {
+      point: "Point 2",
+      desc: (
+        <>
+          해야 할 일을
+          <br />
+          등록하세요
+        </>
+      ),
+      src: "/images/img-main-point2.png",
+      cardStyle: cn("pc:flex-row-reverse"),
+      imgStyle: cn("tablet:px-36 pc:pl-36 pc:pr-10"),
+    },
+  ];
+
   return (
-    <>
-      <div className=""></div>
-    </>
+    <div className="w-full">
+      {pointList.map((item) => (
+        <div
+          key={item.point}
+          className={cn(
+            item.cardStyle,
+            "bg-brand-gray-800 pc:pt-24 mb-[90px] flex flex-col rounded-lg",
+          )}
+        >
+          <div className="tablet:items-start tablet:mb-56 tablet:text-left pc:mb-0 tablet:pl-14 mb-48 flex flex-1 flex-col items-center text-center">
+            <span className="text-2lg text-brand-gray-400 tablet:pb-24 pc:pt-5 block py-14 font-medium">
+              {item.point}
+            </span>
+            <p className="tablet:text-5xl tablet:leading-16 text-4xl leading-12 font-bold">
+              {item.desc}
+            </p>
+          </div>
+          <div className="flex-1">
+            <img src={item.src} alt={item.point} className={cn(item.imgStyle, "h-full w-full")} />
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
