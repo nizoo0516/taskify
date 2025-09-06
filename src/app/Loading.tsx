4c9f52e13b84bd5355133bbeee83bb91f8f6e0dd
Jiii-Eun <mh030231@gmail.com>
2025-09-06 14:48:27 +0900

    Design: 로딩페이지 반응형 추가

--- src/app/Loading.tsx
+++ src/app/Loading.tsx
@@ -1,7 +1,20 @@
+import { cn } from "@/lib/utils/cn";
+
 export default function Loading() {
   return (
     <div className="bg-brand-gray-100 flex h-screen items-center justify-center">
-      <video autoPlay loop muted playsInline preload="auto" className="h-[280px] w-[280px]">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        preload="auto"
+        className={cn(
+          "h-[180px] w-[180px]",
+          "tablet:h-[250px] tablet:w-[250px]",
+          "pc:h-[280px] pc:w-[280px]",
+        )}
+      >
         <source src="/video/Puzzle_Loader.webm" type="video/webm" />
       </video>
     </div>
